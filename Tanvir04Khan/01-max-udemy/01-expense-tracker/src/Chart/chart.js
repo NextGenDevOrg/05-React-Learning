1ad89382b60d37fccaefae6c7068cd4c33d82448
Tanvir04Khan <khantanvirrr98@gmail.com>
2021-10-28 12:28:05 +0530

    sipmlified months array in chart.js

--- Tanvir04Khan/01-max-udemy/01-expense-tracker/src/Chart/chart.js
+++ Tanvir04Khan/01-max-udemy/01-expense-tracker/src/Chart/chart.js
@@ -11,19 +11,21 @@ function Chart(props) {
   });
 
   const months = [
-    { month: "Jan", value: 0 },
-    { month: "Feb", value: 0 },
-    { month: "Mar", value: 0 },
-    { month: "Apr", value: 0 },
-    { month: "May", value: 0 },
-    { month: "Jun", value: 0 },
-    { month: "Jul", value: 0 },
-    { month: "Aug", value: 0 },
-    { month: "Sep", value: 0 },
-    { month: "Oct", value: 0 },
-    { month: "Nov", value: 0 },
-    { month: "Dec", value: 0 },
-  ];
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ].map((month) => {
+    return { month, value: 0 };
+  });
 
   for (const data of datas) {
     const index = data.date.getMonth();
