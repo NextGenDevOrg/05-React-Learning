4ab2f9946ccfa156e0df27b61e2b3c9c3d865828
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-04 14:27:06 +0530

    added-add expense feature

--- Tauqeer-Ahmed-99/01-expense-tracker/src/App.js
+++ Tauqeer-Ahmed-99/01-expense-tracker/src/App.js
@@ -1,19 +1,26 @@
+import React, { useState } from "react";
 import Expenses from "./components/Expenses";
+import NewExpense from "./components/NewExpense";
+
+const expenseData = [
+  { id: 1, title: "Bike", amount: 70000, date: new Date(2020, 5, 13) },
+  { id: 2, title: "MacBook", amount: 100000, date: new Date(2020, 8, 21) },
+  { id: 3, title: "iPhone", amount: 90000, date: new Date(2020, 4, 20) },
+];
 
 function App() {
-  const expenseData = [
-    { id: 1, title: "Car", amount: 600000, date: new Date(2021, 3, 10) },
-    { id: 2, title: "Bike", amount: 70000, date: new Date(2020, 5, 13) },
-    { id: 3, title: "Book", amount: 30, date: new Date(2019, 6, 6) },
-    { id: 4, title: "MacBook", amount: 100000, date: new Date(2020, 8, 21) },
-    { id: 5, title: "iPhone", amount: 90000, date: new Date(2020, 4, 20) },
-    { id: 6, title: "Laptop", amount: 60000, date: new Date(2021, 4, 10) },
-    { id: 7, title: "Watch", amount: 3000, date: new Date(2018, 3, 11) },
-  ];
+  const [oldExpenseData, setExpenseData] = useState(expenseData);
+
+  const addExpenseHandler = (newExpenseData) => {
+    setExpenseData((prevData) => {
+      return [newExpenseData, ...prevData];
+    });
+  };
 
   return (
     <div className="App">
-      <Expenses expensesData={expenseData} />
+      <NewExpense onAddExpense={addExpenseHandler} />
+      <Expenses expensesData={oldExpenseData} />
     </div>
   );
 }
