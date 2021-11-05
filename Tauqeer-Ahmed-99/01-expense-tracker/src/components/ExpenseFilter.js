8dceac4823789ae3b1b0adc29d6ab37af5c3d164
Tauqeer-Ahmed-99 <tauqueerrkhan@gmail.com>
2021-11-05 15:54:27 +0530

    removed filter list glitch

--- Tauqeer-Ahmed-99/01-expense-tracker/src/components/ExpenseFilter.js
+++ Tauqeer-Ahmed-99/01-expense-tracker/src/components/ExpenseFilter.js
@@ -13,13 +13,13 @@ const ExpenseFilter = (props) => {
           <label>Filter by year</label>
           <select onChange={selectionHandler}>
             <option value="All">All</option>
-            <option value="2023">2022</option>
+            <option value="2023">2023</option>
             <option value="2022">2022</option>
             <option value="2021">2021</option>
             <option value="2020">2020</option>
             <option value="2019">2019</option>
-            <option value="2018">2019</option>
-            <option value="2017">2019</option>
+            <option value="2018">2018</option>
+            <option value="2017">2017</option>
           </select>
         </div>
       </div>
